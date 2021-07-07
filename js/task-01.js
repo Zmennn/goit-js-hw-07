afe41f42712d27ee4e68e8489df85e8a99323dd1
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-07-07 23:00:59 +0300

    1+

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,12 @@
-const a = document.querySelectorAll('ul#categories');
-let b = a.children
-console.log(b);
+const categories = document.querySelectorAll('ul#categories>li');
+
+console.log(`В списке ${categories.length} категории.`);
+
+categories.forEach(category => {
+
+    const title = category.querySelector('h2').textContent;
+    console.log(`Категория: ${title}`);
+
+    const quantitySubCategories = category.querySelectorAll('li').length;
+    console.log(`Количество элементов:${quantitySubCategories}`);
+})
