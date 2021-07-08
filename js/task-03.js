374515cab8f9fea1b77241c5a076bcd949804cbf
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-07-08 22:34:42 +0300

    Update task-03.js

---
+++ js/task-03.js
@@ -0,0 +1,22 @@
+const images = [
+    {
+        url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'White and Black Long Fur Cat',
+    },
+    {
+        url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    },
+    {
+        url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'Group of Horses Running',
+    },
+];
+
+const makeStringGalleryMarkup = image => `<li class="gallery--list"><img src="${image.url}" alt="${image.alt}"></li>`;
+
+const makeAllStringGalleryMarkup = images
+    .map(makeStringGalleryMarkup)
+    .join('')
+
+#gallery
