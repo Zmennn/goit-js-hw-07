684a1b88f81d0cc2a4a673df19817d8cc0d8c271
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-07-24 11:56:48 +0300

    11+

---
+++ js/task-11.js
@@ -0,0 +1,25 @@
+
+//создадим ссылку на наблюдаемый объект
+const href = {
+    targetText: document.querySelector('.animated-text')
+};
+
+//создадим функцию, в нее запишем действия которые выполнит браузер в случае выполнения УСЛОВИЙ , определенных
+//самим обсервером и его настройками
+const handleObserver = (entries, observer) => {
+    console.log(entries[0].isIntersecting);
+    if (entries[0].isIntersecting) { href.targetText.classList.add("visible") }
+    else {
+        href.targetText.classList.remove("visible")
+    }
+};
+
+
+//создадим экземпляр обсервера, передадим туда функцию-обработчик, и впишем объект настроек
+const watcher = new IntersectionObserver(handleObserver, { trashHold: 1 });
+
+
+//дадим команду "observe" обсерверу для наблюдения за элементом,
+//включим его так сказать
+watcher.observe(href.targetText)
+
